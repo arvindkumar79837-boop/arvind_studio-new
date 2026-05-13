ac9d50fcbe6565a6fa7cb37c325a3b441fb6d694
Mr. Arvind <arvindkumar79837@gmail.com>
2026-05-13 21:52:43 +0530

    Update main.dart with IDE-style interface

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const ArvindStudioApp());
+  runApp(const ArvindStudio());
 }
 
-class ArvindStudioApp extends StatelessWidget {
-  const ArvindStudioApp({super.key});
+class ArvindStudio extends StatelessWidget {
+  const ArvindStudio({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -13,112 +13,246 @@ class ArvindStudioApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'ARVIND STUDIO',
       theme: ThemeData.dark(),
-      home: const HomeScreen(),
+      home: const IDEHomePage(),
     );
   }
 }
 
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
+class IDEHomePage extends StatefulWidget {
+  const IDEHomePage({super.key});
+
+  @override
+  State<IDEHomePage> createState() => _IDEHomePageState();
+}
+
+class _IDEHomePageState extends State<IDEHomePage> {
+
+  int selectedIndex = 0;
+
+  final List<String> files = [
+    "main.dart",
+    "home.dart",
+    "settings.dart",
+    "pubspec.yaml",
+    "README.md",
+  ];
 
   @override
   Widget build(BuildContext context) {
+
     return Scaffold(
-      backgroundColor: Colors.black,
+
+      backgroundColor: const Color(0xff1e1e1e),
+
       appBar: AppBar(
-        title: const Text("ARVIND STUDIO"),
-        backgroundColor: Colors.black,
-        centerTitle: true,
+        backgroundColor: const Color(0xff252526),
+        title: const Text(
+          "ARVIND STUDIO",
+          style: TextStyle(
+            color: Colors.white,
+          ),
+        ),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(20),
-        child: Column(
-          children: [
 
-            const SizedBox(height: 20),
+      body: Row(
 
-            Container(
-              width: double.infinity,
-              padding: const EdgeInsets.all(20),
-              decoration: BoxDecoration(
-                color: Colors.grey.shade900,
-                borderRadius: BorderRadius.circular(20),
-              ),
-              child: const Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
+        children: [
 
-                  Text(
-                    "Welcome Arvind 👋",
+          // SIDEBAR
+          Container(
+            width: 70,
+            color: const Color(0xff252526),
+
+            child: Column(
+
+              children: [
+
+                sideIcon(Icons.folder, 0),
+                sideIcon(Icons.search, 1),
+                sideIcon(Icons.code, 2),
+                sideIcon(Icons.smart_toy, 3),
+                sideIcon(Icons.settings, 4),
+
+              ],
+            ),
+          ),
+
+          // FILE EXPLORER
+          Container(
+            width: 220,
+            color: const Color(0xff2d2d30),
+
+            child: Column(
+
+              crossAxisAlignment: CrossAxisAlignment.start,
+
+              children: [
+
+                const Padding(
+                  padding: EdgeInsets.all(12),
+                  child: Text(
+                    "EXPLORER",
                     style: TextStyle(
-                      fontSize: 24,
+                      color: Colors.white70,
                       fontWeight: FontWeight.bold,
                     ),
                   ),
+                ),
 
-                  SizedBox(height: 10),
+                Expanded(
+                  child: ListView.builder(
 
-                  Text(
-                    "Build Apps From Mobile",
-                    style: TextStyle(
-                      fontSize: 16,
-                      color: Colors.grey,
-                    ),
+                    itemCount: files.length,
+
+                    itemBuilder: (context, index) {
+
+                      return ListTile(
+
+                        leading: const Icon(
+                          Icons.insert_drive_file,
+                          color: Colors.lightBlueAccent,
+                          size: 20,
+                        ),
+
+                        title: Text(
+                          files[index],
+                          style: const TextStyle(
+                            color: Colors.white,
+                            fontSize: 14,
+                          ),
+                        ),
+
+                        onTap: () {},
+
+                      );
+                    },
                   ),
-                ],
-              ),
+                ),
+              ],
             ),
+          ),
 
-            const SizedBox(height: 30),
+          // EDITOR AREA
+          Expanded(
 
-            Expanded(
-              child: GridView.count(
-                crossAxisCount: 2,
-                crossAxisSpacing: 15,
-                mainAxisSpacing: 15,
+            child: Container(
+
+              color: const Color(0xff1e1e1e),
+
+              child: Column(
+
+                crossAxisAlignment: CrossAxisAlignment.start,
+
                 children: [
 
-                  buildCard(Icons.code, "Code Editor"),
-                  buildCard(Icons.terminal, "Terminal"),
-                  buildCard(Icons.android, "Build APK"),
-                  buildCard(Icons.folder, "Projects"),
-                  buildCard(Icons.smart_toy, "AI Assistant"),
-                  buildCard(Icons.settings, "Settings"),
+                  Container(
+                    height: 45,
+                    color: const Color(0xff2d2d30),
+
+                    child: Row(
+
+                      children: [
+
+                        Container(
+                          padding: const EdgeInsets.symmetric(
+                            horizontal: 15,
+                          ),
+
+                          alignment: Alignment.center,
+
+                          child: const Text(
+                            "main.dart",
+                            style: TextStyle(
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
+
+                      ],
+                    ),
+                  ),
+
+                  const Expanded(
+
+                    child: Padding(
+                      padding: EdgeInsets.all(16),
+
+                      child: SingleChildScrollView(
+
+                        child: Text(
+
+                          '''
+void main() {
+
+  runApp(
+    MyApp(),
+  );
+
+}
+                          ''',
+
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 16,
+                            height: 1.5,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+
+                  Container(
+                    height: 28,
+                    color: Colors.blue,
+
+                    alignment: Alignment.centerLeft,
+
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 12,
+                    ),
+
+                    child: const Text(
+                      "Flutter • Dart • Mobile IDE",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 12,
+                      ),
+                    ),
+                  ),
 
                 ],
               ),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
 
-  Widget buildCard(IconData icon, String title) {
-    return Container(
-      decoration: BoxDecoration(
-        color: Colors.grey.shade900,
-        borderRadius: BorderRadius.circular(20),
-      ),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
+  Widget sideIcon(IconData icon, int index) {
 
-          Icon(
-            icon,
-            size: 50,
-            color: Colors.greenAccent,
-          ),
+    return InkWell(
 
-          const SizedBox(height: 15),
+      onTap: () {
 
-          Text(
-            title,
-            style: const TextStyle(
-              fontSize: 18,
-            ),
-          ),
-        ],
+        setState(() {
+          selectedIndex = index;
+        });
+
+      },
+
+      child: Container(
+
+        height: 65,
+
+        color: selectedIndex == index
+            ? Colors.white10
+            : Colors.transparent,
+
+        child: Icon(
+          icon,
+          color: Colors.white,
+        ),
       ),
     );
   }
