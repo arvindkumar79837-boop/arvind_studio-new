d4379d1ceac46b93d78cfa56066e943814869c86
Mr. Arvind <arvindkumar79837@gmail.com>
2026-05-14 08:49:34 +0530

    Create FileItemModel class for file representation

---
+++ lib/models/file_item_model.dart
@@ -0,0 +1,12 @@
+class FileItemModel {
+
+  final String name;
+  final String path;
+  final bool isFolder;
+
+  FileItemModel({
+    required this.name,
+    required this.path,
+    required this.isFolder,
+  });
+}
